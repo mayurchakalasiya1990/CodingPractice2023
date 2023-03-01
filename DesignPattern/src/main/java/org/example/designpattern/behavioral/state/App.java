e041eca8755c430c90dbefe7d7be36ca5effa225
Mayur Chakalasiya <mayurchakalasiya1990@gmail.com>
2023-03-01 06:23:37 +0000

    State Pattern

---
+++ DesignPattern/src/main/java/org/example/designpattern/behavioral/state/App.java
@@ -0,0 +1,21 @@
+package org.example.designpattern.behavioral.state;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Phone phone = new Phone();
+        simulatePhoneClicks(phone);
+    }
+
+    private static void simulatePhoneClicks(Phone phone) {
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickHome());
+    }
+}
---
+++ DesignPattern/src/main/java/org/example/designpattern/behavioral/state/state/ReadyState.java
@@ -0,0 +1,20 @@
+package org.example.designpattern.behavioral.state.state;
+
+import org.example.designpattern.behavioral.state.Phone;
+
+public class ReadyState extends State{
+    public ReadyState(Phone phone) {
+        super(phone);
+    }
+
+    @Override
+    public String onHome() {
+        return phone.home();
+    }
+
+    @Override
+    public String onOffOn() {
+        phone.setState(new OffState(phone));
+        return phone.lock();
+    }
+}
