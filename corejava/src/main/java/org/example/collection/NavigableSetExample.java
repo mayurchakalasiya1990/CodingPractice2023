ca2276608679e0e43ec126b64fe7499688651aa0
Mayur Chakalasiya <mayurchakalasiya1990@gmail.com>
2023-03-04 22:01:27 +0000

    Record Feature of Java-14

---
+++ corejava/src/main/java/org/example/collection/NavigableSetExample.java
@@ -0,0 +1,29 @@
+package org.example.collection;
+
+import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+/*
+
+headSet() : a view of the original NavigableSet which only contains elements that are "less than" the given element.
+tailSet() : works the same way as the headSet() method, except it returns all elements that are equal to or higher than the given parameter element.
+subSet() :
+
+
+
+ */
+public class NavigableSetExample {
+    public static void main(String[] args) {
+        NavigableSet original = new TreeSet();
+        original.add("1");
+        original.add("2");
+        original.add("3");
+
+        //this headset will contain "1" and "2"
+        SortedSet headset = original.headSet("3");
+        System.out.println("Headset 3:"+headset);
+        //this headset will contain "1", "2", and "3" because "inclusive"=true
+        NavigableSet headset1 = original.headSet("3", true);
+        System.out.println("Headset 3 (inclusive:true):"+headset1);
+    }
+}
---
+++ corejava/src/main/java/org/example/record/RecordWithInstanceMethods.java
@@ -0,0 +1,18 @@
+package org.example.record;
+
+record NewVehicle(String brand, String licensePlate) {
+
+    public String brandAsLowerCase() {
+        return brand().toLowerCase();
+    }
+}
+
+public class RecordWithInstanceMethods {
+
+    public static void main(String[] args) {
+        NewVehicle vehicle=new NewVehicle("TATA","RG-05-AWSZA");
+        System.out.println("Brand:"+vehicle.brandAsLowerCase());
+        System.out.println("licensePlate:"+vehicle.licensePlate());
+        System.out.println("Vehicle:"+vehicle.toString());
+    }
+}
